95e2295397bd164086f848d30be8af4d683f9178
Omar <61359709+omarabusharar@users.noreply.github.com>
2025-10-19 11:10:24 -0700

    Initial Commit

---
+++ Timely/TimelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelyApp.swift
+//  Timely
+//
+//  Created by Omar Abu Sharar on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TimelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
